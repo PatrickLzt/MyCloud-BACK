caaed607a65b5515b82f76ff60628b68f7765d2e
Patrick Lorenzeti <patrick.lorenzeti@mobitec.com.br>
2024-11-12 09:37:30 -0300

    feat: Added api and users files

--- main.go
+++ main.go
@@ -1,39 +1,26 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
-type Server struct {
-	address string
-}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/":
-			w.Write([]byte("index page"))
-			return
-		case "/users":
-			w.Write([]byte("users page"))
-			return
-		default:
-			http.Error(w, "404 Page Not found", http.StatusNotFound)
-			return
-		}
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-}
-
 func main() {
 
-	s := &Server{address: ":8080"}
+	api := &ApiServer{address: ":8080"}
 
-	if err := http.ListenAndServe(s.address, s); err != nil {
-		log.Fatal(err)
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    api.address,
+		Handler: mux,
+	}
+
+	mux.HandleFunc("GET /users", api.getUsersHandler)
+	mux.HandleFunc("POST /users", api.createUsersHandler)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
 	}
 }
