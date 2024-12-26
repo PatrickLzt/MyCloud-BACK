09ccaa28bfacdeffe890c5280be063d3778eb508
Patrick Lorenzeti <patrick.lorenzeti@outlook.com>
2024-12-26 16:47:57 -0300

    alter: Get DB address from environment

--- cmd/db/db.go
+++ cmd/db/db.go
@@ -1,13 +1,14 @@
 package db
 
 import (
+	"github.com/PatrickLzt/MyCloud-BACK/internal/env"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Initialize() *gorm.DB {
 
-	dbURL := "postgres://postgres:21622292a@localhost:5432/db_test?sslmode=disable"
+	dbURL := env.GetString("DB_ADDRESS", "")
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
